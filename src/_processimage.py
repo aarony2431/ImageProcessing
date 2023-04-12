e2325b703575f45f0d3415ea3c057c6e17c6c206
aaron@thinkstation.avititybio <aaron@aviditybio.com>
2023-04-12 16:22:34 -0700

    added logging for struct and memory errors

--- src/_processimage.py
+++ src/_processimage.py
@@ -1,3 +1,4 @@
+import struct
 import time
 import traceback
 from functools import partial
@@ -11,7 +12,13 @@ from tqdm import tqdm
 from . import ImageTIFF
 
 
-def processimage(func: Callable, imagepath: str, isbgr=False, out=None, hierarchy_inputDir=None, tilesize: int = 0,
+def processimage(func: Callable,
+                 imagepath: str,
+                 isbgr=False,
+                 out=None,
+                 hierarchy_inputDir=None,
+                 tilesize: int = 0,
+                 image_library: str = 'dask',
                  **kwargs):
     file = basename(imagepath)
     filebase, ext = [splitext(file)[0], '.tif']
@@ -21,7 +28,7 @@ def processimage(func: Callable, imagepath: str, isbgr=False, out=None, hierarch
         out = '~'
         pass
 
-    if hierarchy_inputDir is not None:
+    if hierarchy_inputDir:
         # Replace the inputDir path with the outputDir path
         imagepath_out = join(out, imagepath.replace(hierarchy_inputDir + sep, ''))
 
@@ -35,39 +42,115 @@ def processimage(func: Callable, imagepath: str, isbgr=False, out=None, hierarch
         pass
 
     if not exists(join(out, newfile)):
-        image = ImageTIFF(imagepath, isbgr=isbgr, tilesize=tilesize)
-        func(image, **kwargs)
+        if image_library.lower() == 'pil':
+            try:
+                img = ImageTIFF(imagepath, isbgr=isbgr, tilesize=tilesize, image_library=image_library)
+                func(img, **kwargs)
 
-        image.saveRGB(out, newfile, ext='')
-        print(f'\'{file}\' was processed and saved as \'{basename(newfile)}\' in \'{out}\'...')
+                img.saveRGB(out, newfile, ext='')
+                print(f'\'{file}\' was processed and saved as \'{basename(newfile)}\' in \'{out}\'...')
+                return None
+            except (MemoryError, struct.error) as err:
+                print(f'memory/struct error: {file}')
+                return imagepath
+        elif image_library.lower() == 'dask':
+            try:
+                img = ImageTIFF(imagepath, isbgr=isbgr, tilesize=tilesize, image_library=image_library)
+                img.map_blocks(func, **kwargs)
+                img.saveDask(out, newfile, ext='')
+                return None
+            except (MemoryError, struct.error) as err:
+                print(f'memory/struct error: {file}')
+                return imagepath
+        else:
+            raise ValueError('Not a valid image library name')
     else:
         # print(f'{n}/{total}: \'{newfile}\' already exists in \'{out}\'!')
         print(f'\'{newfile}\' already exists in \'{out}\'!')
+        return None
+    pass
+
+
+def processimages_loop(func: Callable,
+                       imagepaths: list[str],
+                       logdir=None,
+                       **kwargs):
+    if logdir:
+        logname = f'log{time.time()}.txt'
+        with open(join(logdir, logname), 'w') as f:
+            pass
+        # Loop through each image with progress bar and perform function
+        for imagepath in tqdm(imagepaths, unit='img'):
+            try:
+                out = processimage(func, imagepath, **kwargs)
+                if out is None:
+                    with open(join(logdir, logname), 'a') as logfile:
+                        logfile.write(out)
+                    pass
+            except Exception as e:
+                print(Exception, imagepath, e)
+                print(traceback.format_exc())
+            pass
+        pass
+    else:
+        # Loop through each image with progress bar and perform function
+        for imagepath in tqdm(imagepaths, unit='img'):
+            try:
+                processimage(func, imagepath, **kwargs)
+            except Exception as e:
+                print(Exception, imagepath, e)
+                print(traceback.format_exc())
+            pass
         pass
     pass
 
 
-def processimages_loop(func: Callable, imagepaths: list[str], **kwargs):
-    # Loop through each image with progress bar and perform function
-    for imagepath in tqdm(imagepaths, unit='img'):
-        try:
-            processimage(func, imagepath, **kwargs)
-        except Exception as e:
-            print(Exception, imagepath, e)
-            print(traceback.format_exc())
+def processimages_pool(func: Callable,
+                       imagepaths: list[str],
+                       processes=2,
+                       chunksize=None,
+                       aSync=False,
+                       logdir=None,
+                       **kwargs):
+    if logdir:
+        logname = f'log{time.time_ns()}.txt'
+        with open(join(logdir, logname), 'w') as f:
+            pass
+        with Pool(processes=max(processes, 1)) as pool:
+            if aSync:
+                errs = pool.starmap_async(processimage,
+                                          [(partial(func, **kwargs), imagepath) for imagepath in
+                                           tqdm(imagepaths, unit='img')],
+                                          chunksize=chunksize)
+                for err in errs.get():
+                    if err is None:
+                        with open(join(logdir, logname), 'a') as logfile:
+                            logfile.write(err)
+                            pass
+                        pass
+            else:
+                for err in pool.starmap(processimage,
+                                        [(partial(func, **kwargs), imagepath) for imagepath in
+                                         tqdm(imagepaths, unit='img')],
+                                        chunksize=chunksize):
+                    if err is None:
+                        with open(join(logdir, logname), 'a') as logfile:
+                            logfile.write(err)
+                            pass
+                        pass
+            pass
         pass
-    pass
-
-
-def processimages_pool(func: Callable, imagepaths: list[str], processes=2, chunksize=None, aSync=False, **kwargs):
-    # Create worker pools and process images with progress bar
-    with Pool(processes=max(processes, 1)) as pool:
-        if aSync:
-            pool.starmap_async(processimage,
-                               [(partial(func, **kwargs), imagepath) for imagepath in tqdm(imagepaths, unit='img')],
-                               chunksize=chunksize)
-        else:
-            pool.starmap(processimage,
-                         [(partial(func, **kwargs), imagepath) for imagepath in tqdm(imagepaths, unit='img')],
-                         chunksize=chunksize)
+    else:
+        # Create worker pools and process images with progress bar
+        with Pool(processes=max(processes, 1)) as pool:
+            if aSync:
+                pool.starmap_async(processimage,
+                                   [(partial(func, **kwargs), imagepath) for imagepath in tqdm(imagepaths, unit='img')],
+                                   chunksize=chunksize)
+            else:
+                pool.starmap(processimage,
+                             [(partial(func, **kwargs), imagepath) for imagepath in tqdm(imagepaths, unit='img')],
+                             chunksize=chunksize)
+            pass
+        pass
     pass
