922f863f75a5b55877f42d2c9665a87e8973f29c
aaron@thinkstation.avititybio <aaron@aviditybio.com>
2023-05-03 14:47:39 -0700

    create test cases for Huang Thresholding (without tiling)

--- src/ImageJ/_threshold.py
+++ src/ImageJ/_threshold.py
@@ -7,10 +7,12 @@ import tifffile as tiff
 def threshold_huang(
         image: os.PathLike | str,
         color_channel: int | str,
-        tile_size: tuple[int, int] | None = None
-        ) -> np.ndarray:
+        tile_size: tuple[int, int] | None = None,
+        white_background: bool = True
+) -> np.ndarray:
     """
 
+    :param white_background:
     :param image:
     :param color_channel:
     :param tile_size:
@@ -43,57 +45,70 @@ def threshold_huang(
         tiles_tall = np.arange(0, height, tile_size[0])
         tiles_wide = np.arange(0, width, tile_size[1])
 
+        # Load the TIFF image into memory-mapped data
+        mmap_data = np.memmap(image, shape=shape, mode='r')
+
         # loop over the image in chunks
         for [y, x] in np.ndindex(len(tiles_tall), len(tiles_wide)):
             # define tile coordinates
             start_height, start_width = (y * tile_size[0], x * tile_size[1])
             end_height, end_width = (start_height + tile_size[0], start_width + tile_size[1])
 
-            # Load the TIFF image into memory-mapped data
-            data = np.memmap(image, shape=shape)[start_height:end_height, start_width:end_width, channel]
+            data = mmap_data[start_height:end_height, start_width:end_width, channel]
 
-            # Calculate the histogram of the image
-            hist, bins = np.histogram(data, bins=256)
+            # apply Huang's thresholding method
+            hist, bins = np.histogram(data.astype(np.uint8).ravel(), 256, [0, 256])
+            hist_norm = hist.astype('float') / hist.max()
 
-            # Calculate the cumulative distribution function (CDF) of the histogram
-            cdf = np.cumsum(hist)
+            omega = np.cumsum(hist_norm)
+            mu = np.cumsum(hist_norm * np.arange(0, 256))
+            mu_t = mu[-1]
 
-            # Calculate the probabilities of the pixels belonging to the foreground and background using the CDF
-            probs = hist / float(cdf[-1])
+            # avoid divide by zero warning
+            eps = np.finfo(float).eps
+            omega[omega == 0] = eps
+            omega[omega == 1] = 1 - eps
 
-            # Calculate the entropies of the foreground and background regions for each possible threshold value
-            entropies = -probs * np.log2(probs) - (1 - probs) * np.log2(1 - probs)
+            # calculate the between-class variance
+            i = np.arange(0, 256)
+            w0 = omega[i]
+            w1 = 1 - w0
+            mu_0 = mu[i] / w0
+            mu_1 = (mu_t - mu[i]) / w1
+            s = w0 * w1 * ((mu_0 - mu_1) ** 2)
+            best_thresh = np.argmax(s)
 
-            # Find the threshold value that maximizes the sum of the entropies
-            threshold = np.argmax(entropies)
+            print(best_thresh)
 
-            # Apply the threshold to the image to obtain the binary image
-            binary[start_height:end_height, start_width:end_width] = data > threshold
-
-            del data
+            binary[start_height:end_height, start_width:end_width] = \
+                data < best_thresh if white_background else data > best_thresh
             pass
+        del mmap_data
         pass
     else:
-        # Load the TIFF image into memory-mapped data
-        data = np.memmap(image, shape=shape)[..., channel]
+        data = tiff.imread(image)[..., channel]
 
-        # Calculate the histogram of the image
-        hist, bins = np.histogram(data, bins=256)
+        # apply Huang's thresholding method
+        hist, bins = np.histogram(data.astype(np.uint8).ravel(), 256, [0, 256])
+        hist_norm = hist.astype('float') / hist.max()
 
-        # Calculate the cumulative distribution function (CDF) of the histogram
-        cdf = np.cumsum(hist)
+        omega = np.cumsum(hist_norm)
+        mu = np.cumsum(hist_norm * np.arange(0, 256))
+        mu_t = mu[-1]
 
-        # Calculate the probabilities of the pixels belonging to the foreground and background using the CDF
-        probs = hist / float(cdf[-1])
+        # avoid divide by zero warning
+        eps = np.finfo(float).eps
+        omega[omega == 0] = eps
+        omega[omega == 1] = 1 - eps
 
-        # Calculate the entropies of the foreground and background regions for each possible threshold value
-        entropies = -probs * np.log2(probs) - (1 - probs) * np.log2(1 - probs)
+        # calculate the between-class variance
+        i = np.arange(0, 256)
+        w0 = omega[i]
+        w1 = 1 - w0
+        mu_0 = mu[i] / w0
+        mu_1 = (mu_t - mu[i]) / w1
+        s = w0 * w1 * ((mu_0 - mu_1) ** 2)
+        best_thresh = np.argmax(s)
 
-        # Find the threshold value that maximizes the sum of the entropies
-        threshold = np.argmax(entropies)
-
-        # Apply the threshold to the image to obtain the binary image
-        binary = data > threshold
-        del data
-        pass
+        binary = data < best_thresh if white_background else data > best_thresh
     return binary
--- test/_test_ImageJ.py
+++ test/_test_ImageJ.py
@@ -1,18 +1,19 @@
 import inspect
 
-from os.path import dirname, join
+from os.path import dirname, join, splitext
+
+import numpy as np
+from PIL import Image
 from ..src.ImageJ import threshold_huang, ParticleAnalyzer, FindMaxima
 
 
 def test_imagej():
+    _test_threshold()
     _test_maxima()
     pass
 
 
 def _test_maxima():
-    # Test imagepath
-    image_path = r"F:\Aaron Y - extra space\Orbit H&E\Cyno TIF annotations - output\too big\uncompressed" \
-                 r"\SP-012102_1001.22 adrenal gland_Mfa-SSB-O1_Default_Extendedlf_UNCOMPRESSED.tif"
     test_image = r"SP-012102_1001.22 adrenal gland_Mfa-SSB-O1_Default_Extendedlf_Processed.tif"
 
     # folder_path = dirname(inspect.getfile(test_maxima))
@@ -32,4 +33,24 @@ def _test_maxima():
     assert 639082 == FindMaxima(image_path, color_channel='red', noise_tolerance=1.0)
     assert 639116 == FindMaxima(image_path, color_channel='red', noise_tolerance=1.0,
                                 tile_size=(chunk_size, chunk_size))
+    pass
+
+
+def _test_threshold():
+    test_image = r"SP-012102_1001.22 adrenal gland_Mfa-SSB-O1_Default_Extendedlf_UNCOMPRESSED.tif"
+
+    # folder_path = dirname(inspect.getfile(test_maxima))
+    folder_path = r"F:\Aaron Y - extra space\Orbit H&E\Cyno TIF annotations - output\too big\uncompressed"
+    image_path = join(folder_path, test_image)
+
+    # Threshold test
+    thresholded_image_array = threshold_huang(image_path, color_channel='blue')
+    assert 92304771 == np.count_nonzero(thresholded_image_array)
+    # currently not working in chunks
+    chunk_size = int(2 ** 12)
+    thresholded_image_array = threshold_huang(image_path, color_channel='blue', tile_size=(chunk_size, chunk_size))
+    filebase, ext = splitext(test_image)
+    threshold_image = join(folder_path, f'{filebase}_THRESHOLD{ext}')
+    Image.fromarray(thresholded_image_array).save(threshold_image)
+    print(np.count_nonzero(thresholded_image_array))
     pass
